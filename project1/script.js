241e480d98e220c211122ef83562793625c773d3
Noeeekr <cardozoandre0101@gmail.com>
2023-12-02 00:34:31 -0400

    hover effect project

--- project1/script.js
+++ project1/script.js
@@ -1,1 +1,19 @@
-/**/
+const cardsHoverEffect = (event) => {
+    const { currentTarget: target } = event;
+    console.log(event,target) 
+
+    const rect = target.getBoundingClientRect();
+        x = event.clientX - rect.left;
+        y = event.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+}
+
+for (const card of document.querySelectorAll(".card")) {
+    card.onmousemove = (event) => cardsHoverEffect(event);
+    let randomColor = `${Math.floor(Math.random() * 250) + 1},${Math.floor(Math.random() * 250) + 3},${Math.floor(Math.random() * 250) + 1}`
+    card.style.setProperty("--random-color", `rgb(${randomColor},${0.3})`);
+    card.style.setProperty("--random-color-translucent", `rgba(${randomColor},${0.09})`);
+}
+/* makes a card array and put the event listener with a function that does the mouse effect */
