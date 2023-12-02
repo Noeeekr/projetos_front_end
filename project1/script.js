1ad0e0d045a74e928977be9d50b784ce1adf5dec
Noeeekr <cardozoandre0101@gmail.com>
2023-12-02 17:59:35 -0400

    changes on how effect works + performace improvement

--- project1/script.js
+++ project1/script.js
@@ -1,19 +1,29 @@
+let lastMoment = Date.now();
+
 const cardsHoverEffect = (event) => {
-    const { currentTarget: target } = event;
-    console.log(event,target) 
+    console.log("função executada")
+    if (lastMoment + 30 > Date.now()) {return;} else {
+        lastMoment = Date.now();
+    } /* gets the last listened moment and checks if 30ms already passed before executing the function */
 
-    const rect = target.getBoundingClientRect();
-        x = event.clientX - rect.left;
-        y = event.clientY - rect.top;
+    document.querySelectorAll(".card").forEach((card) => {
+        const rect = card.getBoundingClientRect();
+            x = event.clientX - rect.left;
+            y = event.clientY - rect.top;
 
-    target.style.setProperty("--mouse-x", `${x}px`);
-    target.style.setProperty("--mouse-y", `${y}px`);
-}
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    })
+};
 
 for (const card of document.querySelectorAll(".card")) {
-    card.onmousemove = (event) => cardsHoverEffect(event);
-    let randomColor = `${Math.floor(Math.random() * 250) + 1},${Math.floor(Math.random() * 250) + 3},${Math.floor(Math.random() * 250) + 1}`
+    let randomColor = `${Math.floor(Math.random() * 210) + 40},${Math.floor(Math.random() * 210) + 40},${Math.floor(Math.random() * 210) + 40}`
     card.style.setProperty("--random-color", `rgb(${randomColor},${0.3})`);
     card.style.setProperty("--random-color-translucent", `rgba(${randomColor},${0.09})`);
-}
+};
+
+(function(){
+    document.getElementsByTagName("body")[0].onmousemove = (e) => cardsHoverEffect(e);
+})();
+
 /* makes a card array and put the event listener with a function that does the mouse effect */
