2bd716d7b2ec4a255073ddf3a9b7493befcf66cd
Noeeekr <cardozoandre0101@gmail.com>
2023-12-17 18:50:04 -0400

    removing some nesting to make code looks better

--- taskManager1/script.js
+++ taskManager1/script.js
@@ -34,9 +34,72 @@ function Task(arrayOfValues) {
     this.updated = null;
 }
 /* our object constructor function that makes new tasks*/
+function editButtonAction(e){
+    let editButton = e.target;
+    let task = e.target;
+    while (!task.getAttribute("task")) {
+        task = task.parentElement;
+    }
+
+    if (editButton.className.includes("taskIconActive")) {
+        let taskArray = [...document.querySelectorAll(".task")]
+
+        taskHolder[taskArray.indexOf(task)].name = task.querySelector("input.nameInput").value;
+        taskHolder[taskArray.indexOf(task)].description = task.querySelector(".descriptionInput").value;
+        taskHolder[taskArray.indexOf(task)].priority = task.querySelector(".priorityInput").value;
+        // could be replaced for forEach in future;
+        task.querySelector(".task-title").textContent = task.querySelector("input.nameInput").value;
+        task.querySelector(".task-description").textContent = task.querySelector(".descriptionInput").value;
+        task.querySelector(".task-priority").textContent = task.querySelector(".priorityInput").value;
+        
+        updateTaskData()
+        
+        task.querySelector(".task-editContainer").remove()
+        task.querySelector(".task .taskIcon:nth-child(1)").classList.remove("taskIconActive");
+    } else {
+        let editContainer = document.createElement("div");
+            editContainer.className = "task-editContainer";
+
+        let name = document.createElement("input");
+            name.className = "nameInput";
+            name.value = task.querySelector(".task-title").textContent;
+        let priority = document.createElement("select");
+            priority.className = "priorityInput";
+            let options = document.getElementById("taskForm-priorityInput").children;
+                for (let i = 0; i < options.length; ++i) {
+                    let opt = document.createElement("option");
+                    opt.value = options[i].value;
+                    opt.textContent = options[i].value;
+                    priority.appendChild(opt);
+                }
+            priority.value = task.querySelector(".task-priority").textContent;
+        let description = document.createElement("textarea");
+            description.className = "descriptionInput";
+            description.textContent = task.querySelector(".task-description").textContent;
+
+        editContainer.append(name,priority,description);
+
+        task.appendChild(editContainer);
+        task.querySelector(".task .taskIcon:nth-child(1)").classList.add("taskIconActive");
+    }
+}
+/* makes the edit Button  event listener work */
+function closeButtonAction(e){
+    let task = e.target;
+    while (task.classList[0] !== "task") {
+        task = task.parentElement;
+    }
+    
+    taskHolder.splice([...document.querySelectorAll(".task")].indexOf(task),1);
+    updateTaskData();
+
+    task.remove();
+}
+/* makes the close Button event listener work */
 function renderTaskOnTaskViewer(taskObject) {
     let task = document.createElement("div");
         task.className = "task";
+        task.setAttribute("task","true");
 
     let title = document.createElement("div");
         title.className = "task-titleWrapper";
@@ -58,68 +121,11 @@ function renderTaskOnTaskViewer(taskObject) {
         let editButton = document.createElement("span");
             editButton.className = "material-symbols-outlined taskIcon";
             editButton.textContent = "settings";
-            editButton.addEventListener("click",function(e){
-                if (editButton.className.includes("taskIconActive")) {
-                    let taskArray = [...document.querySelectorAll(".task")]
-
-                    taskHolder[taskArray.indexOf(task)].name = task.querySelector("input.nameInput").value;
-                    taskHolder[taskArray.indexOf(task)].description = task.querySelector(".descriptionInput").value;
-                    taskHolder[taskArray.indexOf(task)].priority = task.querySelector(".priorityInput").value;
-                    // could be replaced for forEach in future;
-                    task.querySelector(".task-title").textContent = task.querySelector("input.nameInput").value;
-                    task.querySelector(".task-description").textContent = task.querySelector(".descriptionInput").value;
-                    task.querySelector(".task-priority").textContent = task.querySelector(".priorityInput").value;
-                    
-                    updateTaskData()
-                    
-                    task.querySelector(".task-editContainer").remove()
-                    task.querySelector(".task .taskIcon:nth-child(1)").classList.remove("taskIconActive");
-                } else {
-                    let task = e.target;
-                    while (task.classList[0] !== "task") {
-                        task = task.parentElement;
-                    }
-
-                    let editContainer = document.createElement("div");
-                        editContainer.className = "task-editContainer";
-
-                    let name = document.createElement("input");
-                        name.className = "nameInput";
-                        name.value = task.querySelector(".task-title").textContent;
-                    let priority = document.createElement("select");
-                        priority.className = "priorityInput";
-                        let options = document.getElementById("taskForm-priorityInput").children;
-                            for (let i = 0; i < options.length; ++i) {
-                                let opt = document.createElement("option");
-                                opt.value = options[i].value;
-                                opt.textContent = options[i].value;
-                                priority.appendChild(opt);
-                            }
-                        priority.value = task.querySelector(".task-priority").textContent;
-                    let description = document.createElement("textarea");
-                        description.className = "descriptionInput";
-                        description.textContent = task.querySelector(".task-description").textContent;
-
-                    editContainer.append(name,priority,description);
-
-                    task.appendChild(editContainer);
-                    task.querySelector(".task .taskIcon:nth-child(1)").classList.add("taskIconActive");
-                }
-            })
+            editButton.addEventListener("click",function(e){editButtonAction(e)})
         let closeButton = document.createElement("span");
             closeButton.className = "material-symbols-outlined taskIcon";
             closeButton.textContent = "done";
-            closeButton.addEventListener("click",function(e){
-                let task = e.target;
-                while (task.classList[0] !== "task") {
-                    task = task.parentElement;
-                }
-                
-                taskHolder.splice([...document.querySelectorAll(".task")].indexOf(task),1);
-                updateTaskData();
-
-                task.remove();
-            })
+            closeButton.addEventListener("click",function(e){closeButtonAction(e)})
     iconWrapper.append(editButton,closeButton);
     title.append(iconWrapper,name);
 
@@ -134,21 +140,12 @@ function getTaskForm(){
 function validateTaskFormValues(arrayOfValues){
     if (arrayOfValues.includes("")) {
         document.getElementById("warningPopUp-closeButton").classList.add("active");
-    } else {
-        taskHolder.push(new Task(arrayOfValues)) && updateTaskData();
-        renderTaskOnTaskViewer(taskHolder[taskHolder.length - 1]);
+        return;
     }
+
+    taskHolder.push(new Task(arrayOfValues)) && updateTaskData();
+    renderTaskOnTaskViewer(taskHolder[taskHolder.length - 1]);
+    /* adds task to array, update the cookie and render the task that just got added */
     document.getElementsByName("taskForm-value").forEach((node) => {if (node) {node.value = "";}});
 }
 /* the function that validates the values and clean the fields*/
-
-
-
-/* 
-delete later!!
-
-to-do:
-
-    css: change the select menu;
-    ui: mobile acessibility;
-*/
