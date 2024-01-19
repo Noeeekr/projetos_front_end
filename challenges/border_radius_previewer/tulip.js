299f3971f2baf588b2479703469d8d761cf7b8a3
Noeeekr <cardozoandre0101@gmail.com>
2024-01-18 22:39:21 -0400

    project is functional + design not done

--- challenges/border_radius_previewer/tulip.js
+++ challenges/border_radius_previewer/tulip.js
@@ -7,14 +7,18 @@ let infos = (function(){
 })();
 
 ;(function(){
-    [...document.querySelectorAll("range")].forEach(
+    [...document.querySelectorAll(".range")].forEach(
         (input) => {
-            input.addEventListener("input",update,false);
-            input.addEventListener("change",update,false);
+            input.addEventListener("input",function(e){update(e)},false);
+            input.addEventListener("change",function(e){update(e)},false);
         }
-        )
+    )
 })();
 
-function update() {
+function update(event) {
+    let inputs = [...document.querySelectorAll(".range")];
+    let index = [...document.querySelectorAll(".range")].indexOf(event.target);
 
+    document.querySelector(".box").style.setProperty(`--${inputs[index].getAttribute("name")}`,`${inputs[index].value}%`)
+    document.querySelector(".box .inputValueViewer").textContent = `${inputs[index].getAttribute("name")} ${inputs[index].value}%`;
 }
