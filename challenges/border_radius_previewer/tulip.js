f1af5ebb45535e73b52dc37f57f92d53ce201fce
Noeeekr <cardozoandre0101@gmail.com>
2024-01-19 23:15:50 -0400

    minor changes in the way the range inputs work

--- challenges/border_radius_previewer/tulip.js
+++ challenges/border_radius_previewer/tulip.js
@@ -9,16 +9,17 @@ let infos = (function(){
 ;(function(){
     [...document.querySelectorAll(".range")].forEach(
         (input) => {
-            input.addEventListener("input",function(e){update(e)},false);
-            input.addEventListener("change",function(e){update(e)},false);
+            input.addEventListener("input",function(e){update(e.target)},false);
+            input.addEventListener("change",function(e){update(e.target)},false);
+            update(input)
         }
     )
 })();
 
-function update(event) {
+function update(target) {
     let inputs = [...document.querySelectorAll(".range")];
-    let index = [...document.querySelectorAll(".range")].indexOf(event.target);
+    let index = [...document.querySelectorAll(".range")].indexOf(target);
 
     document.querySelector(".box").style.setProperty(`--${inputs[index].getAttribute("name")}`,`${inputs[index].value}%`)
-    document.querySelector(".box .inputValueViewer").textContent = `${inputs[index].getAttribute("name")} ${inputs[index].value}%`;
+    document.querySelector(".box .inputValueViewer").textContent = `${inputs[index].getAttribute("name").slice(0,-1)} ${inputs[index].value}%`;
 }
