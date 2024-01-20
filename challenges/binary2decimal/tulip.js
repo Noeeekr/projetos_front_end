d87a58dff9c09088e763da88ddf8c2220cd99a63
Noeeekr <cardozoandre0101@gmail.com>
2024-01-19 23:15:29 -0400

    new project (binary to decimal) + initial desing

---
+++ challenges/binary2decimal/tulip.js
@@ -0,0 +1,15 @@
+// add event listener to form submit;
+
+// add a check result button for mobile users
+
+// add event listener for input for when people digit 
+// more than one char it'll send it to the next input
+// and do that until all inputs are filled then submit
+
+// add popup message when people insert a value bigger 
+// than expected
+
+// remove the min/max value after that in html input;
+
+// use exponentiation to make the account to binary
+// conversion
