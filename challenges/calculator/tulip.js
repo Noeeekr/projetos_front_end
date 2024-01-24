2e9b077bd2876b59d9e8f175ab6753fea9d2c88e
Noeeekr <cardozoandre0101@gmail.com>
2024-01-24 12:41:12 -0400

    bug fixes + responsive layout + 0 bug still there

--- challenges/calculator/tulip.js
+++ challenges/calculator/tulip.js
@@ -1,6 +1,7 @@
 let infos = (function(){
-    let currentMath = [""]; // holds the current math that's being done
-    let currentValue = null; // holds the full math expression;
+    let currentMath = [""]; // holds the current math numbers and operators for view and calc;
+    let currentValue = null; // holds the current value of the math for view;
+    let calc = null; // copy of current math that's used to process the operations;
     let operationPriority = {
         "+": 1,
         "-": 1,
@@ -8,13 +9,13 @@ let infos = (function(){
         "X": 2,
     };
 
-    return {currentMath,currentValue,operationPriority}
+    return {calc,currentMath,currentValue,operationPriority}
 })();
 (function(){
     addButtonListener(
         [...document.querySelector(".calculatorButtons").children],
         "click",
-        checkMath
+        mathController
     )
 })();
 
@@ -22,87 +23,58 @@ let infos = (function(){
 function addButtonListener(buttonArray,event,func) {
     buttonArray.forEach((button) => {button.addEventListener(event,func)})
 }
-function finishMath() {
-    calc();
-    // sends the full result to historic;
-    // mathCalc();
-    // cleanAll();
-    // etc;
+function renderMath() {
+    let renderEntireCalc = document.querySelector(".currentCalc");
+    let renderResult = document.querySelector(".currentResult");
+
+    let text = infos.currentMath.map((value) => {if (typeof value === "object") {return value.value} return value});
+    renderEntireCalc.textContent = text.join(" ").toLowerCase(); 
+    
+    if (infos.currentValue.length === 1) renderResult.textContent = infos.currentValue;
 }
+function addOlderResult() {
+    let olderResultLocal = document.querySelector(".olderResults .result.currentResult");
+    let olderResult = `<div class="result">${infos.currentValue}</div>`
+
+    olderResultLocal.insertAdjacentHTML("afterend",olderResult);
+}
+
+// special button handlers;
 function cleanAll() {
     infos.currentMath = [""];
+    infos.currentValue = [""];
+    infos.calc = null;
 }
 function cleanLast() {
-    if (infos.currentMath[infos.currentMath.length - 1].length === 0) {
+    let lastIndex = infos.currentMath[infos.currentMath.length - 1];
+    if (!lastIndex && infos.currentMath.length > 1) {
         infos.currentMath.splice(-2);
         return;
     } 
-    let currentValueArray = infos.currentMath[infos.currentMath.length - 1].split("");
+
+    let currentValueArray = lastIndex.split("");
     currentValueArray.pop()
 
     infos.currentMath[infos.currentMath.length - 1] = currentValueArray.join("");
 }
-function renderMath(text) {
-    let renderLocal = document.querySelector(".currentResult");
-
-    renderLocal.textContent = text;  
+function finishMath() {
+    calc();
+    addOlderResult();
+    cleanAll();
+    renderMath();
 }
-// actual math handlers;
+// calc handler;
 function calc() {
-    let calc = [...infos.currentMath];
+    infos.calc = [...infos.currentMath];
 
-    while (findOperation(calc,2)) {
-        let [operationIndex,operation] = findOperation(calc,2);
+    while (findOperation(infos.calc,2)) infos.calc = type2operation(infos.calc);
 
-        if (calc[operationIndex - 1] && calc[operationIndex + 1]) {
-            let result = operation.value === "X" ? multiply(calc[operationIndex -1],calc[operationIndex + 1]) : divide(calc[operationIndex -1],calc[operationIndex + 1]);
-        
-            calc[operationIndex + 1] = result;
-            calc.splice(operationIndex - 1,operationIndex + 1)
-        }
-    }
+    while (findOperation(infos.calc,1)) infos.calc = type1operation(infos.calc);
 
-    while (findOperation(calc,1)) {
-        let [operationIndex,operation] = findOperation(calc,1);
-
-        if (calc[operationIndex - 1] && calc[operationIndex + 1]) {
-            let result = operation.value === "+" ? sum(calc[operationIndex -1],calc[operationIndex + 1]) : decrease(calc[operationIndex -1],calc[operationIndex + 1]);
-        
-            calc[operationIndex + 1] = result;
-            calc.splice(operationIndex - 1,operationIndex + 1)
-        }
-    }
-
-    infos.currentValue = calc;
-    console.log(infos.currentValue)
+    infos.currentValue = infos.calc;
 }
-function findOperation(calc,priority) {
-    let operation = calc.find((operation) => {if (operation.priority) return operation.priority === priority})
-    let operationIndex = calc.findIndex((value) => {return value === operation});
-    
-    if (!(operation && calc[operationIndex + 1] && calc[operationIndex + 1])) return false;
 
-    return [operationIndex,operation];
-}
-function checkMath(event) {
-    let buttonContent_isNumber = parseInt(event.target.textContent) ? true : false;
-    let buttonContent = parseInt(event.target.textContent) ? parseInt(event.target.textContent) : event.target.textContent;
-    
-    if (checkSpecialValues(buttonContent)) {
-        renderMath(infos.currentMath.join(""));
-        return;
-    }
-    
-    let currentValue = infos.currentMath[infos.currentMath.length - 1];
-
-    if (buttonContent_isNumber &&  currentValue.length > 7) return; // number size limit;
-    if (!buttonContent_isNumber && infos.currentMath[0].length === 0) return; // no start with operations;
-    
-    buttonContent_isNumber ? addNumber(buttonContent) : addOperation(currentValue,buttonContent); 
-    
-    renderMath(infos.currentMath)
-    calc()
-}
+// operation handler;
 function checkSpecialValues(buttonContent) {
     switch (buttonContent) {
         case "C":
@@ -118,6 +90,39 @@ function checkSpecialValues(buttonContent) {
             return false;
     }
 }
+function findOperation(calc,priority) {
+    let operation = calc.find((operation) => {if (operation.priority) return operation.priority === priority})
+    let operationIndex = calc.findIndex((value) => {return value === operation});
+    
+    if (!(operation && calc[operationIndex + 1] && calc[operationIndex + 1])) return false;
+
+    return [operationIndex,operation];
+}
+function type2operation(calc) {
+    let [operationIndex,operation] = findOperation(calc,2);
+
+    if (calc[operationIndex - 1] && calc[operationIndex + 1]) {
+        let result = operation.value === "X" ? multiply(calc[operationIndex -1],calc[operationIndex + 1]) : divide(calc[operationIndex -1],calc[operationIndex + 1]);
+    
+        calc[operationIndex + 1] = result;
+        calc.splice(operationIndex - 1,operationIndex + 1)
+    }
+
+    return calc;
+}
+function type1operation(calc) {
+    let [operationIndex,operation] = findOperation(calc,1);
+
+    if (calc[operationIndex - 1] && calc[operationIndex + 1]) {
+        let result = operation.value === "+" ? sum(calc[operationIndex -1],calc[operationIndex + 1]) : decrease(calc[operationIndex -1],calc[operationIndex + 1]);
+    
+        calc[operationIndex + 1] = result;
+        calc.splice(operationIndex - 1,operationIndex + 1)
+    }
+
+    return calc;
+}
+
 function addNumber(buttonContent) {
     infos.currentMath[infos.currentMath.length - 1] = infos.currentMath[infos.currentMath.length - 1] + buttonContent;
     return;
@@ -129,6 +134,29 @@ function addOperation(currentValue,buttonContent) {
     } 
     infos.currentMath[infos.currentMath.length] = {value: buttonContent, priority: infos.operationPriority[buttonContent]};
     infos.currentMath.push("");
+}
+
+
+// calculator controller
+function mathController(event) {
+    let buttonContent_isNumber = parseInt(event.target.textContent) ? true : false;
+    let buttonContent = parseInt(event.target.textContent) ? parseInt(event.target.textContent) : event.target.textContent;
+    
+    if (checkSpecialValues(buttonContent)) {
+        calc() 
+        renderMath()
+        return;
+    }
+    
+    let currentValue = infos.currentMath[infos.currentMath.length - 1];
+
+    if (buttonContent_isNumber && currentValue.length > 7) return; // number size limit;
+    if (!buttonContent_isNumber && infos.currentMath[0].length === 0) return; // no start with operations;
+    
+    buttonContent_isNumber ? addNumber(buttonContent) : addOperation(currentValue,buttonContent); 
+    
+    calc()
+    renderMath()
 }
 // operations ;;
 function multiply(a,b) {
@@ -145,6 +173,4 @@ function decrease(a,b) {
 }
 
 
-// pelo amor de deus ajeita esse codigo andré que porra que é isso que voce fez
-// caralho eu cometi atrocidades nesse codigo pqp me mata 
-
+// fix zero value bug
