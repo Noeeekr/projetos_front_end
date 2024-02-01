1d7ae5b00d81be65605e1ddaadd1e3d7b09bb74d
Noeeekr <cardozoandre0101@gmail.com>
2024-02-01 00:40:48 -0400

    base project done, full working + some features are meant to be added later

--- schoolReportCard/edit.js
+++ schoolReportCard/edit.js
@@ -46,24 +46,65 @@ let addSubjMechanism = document.querySelector(".form.addSubject");
 
 function updateStudent() {
     localStorage.setItem("studentsData",JSON.stringify(studentService.students));
+    
+    let studentSubjects = studentService.students[currentStudentID].subjects;    
+
+    studentService.clearBody();
+    studentService.renderHeader(studentSubjects)
     studentService.renderStudent(currentStudentID);
+
+    enableGradeEdit()
 }
 function enableGradeEdit() {
     [...document.querySelectorAll(".tbody .td.edit")].forEach((studentGrade) => {
-        studentGrade.addEventListener("click",(e) => {startChangeGradeProcess(e)})
-    })
+        studentGrade.addEventListener("click",(e) => {startCGP(e)})
+    });
+    document.querySelector(".changeGradePopupHolder").addEventListener("click",(e) => {interruptCGP(e)});
+    document.querySelector(".changeGradePopupHolder .button").addEventListener("click",(e) => {finishCGP(e)})
 }
-function startChangeGradeProcess(event) {
-    let gradeSubject = event.target.getAttribute("subject").toUpperCase();
+// CGP = ChangeGradeProcess
+function startCGP(event) {
+    let subject = event.target.getAttribute("subject").toUpperCase();
+
     let popup = document.querySelector(".changeGradePopupHolder");
         popup.className += " active";
-    let subjectPoints = studentService.students[currentStudentID].subjects[gradeSubject];
 
-    console.log(subjectPoints)
+    popup.querySelector(".popupInfo .studentName").textContent = studentService.students[currentStudentID].name
+    popup.querySelector(".popupInfo .studentSubject").textContent = subject;
+    popup.querySelector(".grade").value = studentService.students[currentStudentID].subjects[subject];
+}
+function interruptCGP(event) {
+    if (event.target.classList[0] !== "changeGradePopupHolder") return;
+
+    event.target.classList.remove("active");
+
+    let grade = event.target.parentElement.querySelector("input");
+        grade.value = 0;
+
+    studentService.clearBody();
+    studentService.renderStudent(currentStudentID);
+    
+    enableGradeEdit();
+}
+function finishCGP(event) {
+    let subject = event.target.parentElement.querySelector(".popupInfo .studentSubject").textContent.toUpperCase();
+    let grade = event.target.parentElement.querySelector("input");
+    let gradeValue = parseFloat(grade.value);
+
+    if (gradeValue > 100 || gradeValue < 0) {
+        grade.style = "border-bottom: red 2px solid"
+        return;
+    }
+
+    studentService.students[currentStudentID].subjects[subject] = gradeValue;
+    
+    let popup = document.querySelector(".changeGradePopupHolder");
+        popup.classList.remove("active");
+
+    grade.value = 0;
 }
 // buttons ;
 
 document.querySelector(".buttonHolder.submit .addSubject").addEventListener("click",addSubject);
 document.querySelector(".buttonHolder.searchStudent .button").addEventListener("click",searchStudent);
 document.querySelector(".buttonHolder.submit .updateGrade").addEventListener("click",updateStudent);
-
